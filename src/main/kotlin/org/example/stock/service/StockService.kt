a28fa721c152dee74047a8b4ec4c62636dcad880
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-11-07 20:35:31 +0900

    feat: @Syncronized를 사용하여, Race Condition 해결

--- src/main/kotlin/org/example/stock/service/StockService.kt
+++ src/main/kotlin/org/example/stock/service/StockService.kt
@@ -9,8 +9,9 @@ class StockService (
     private val stockJpaRepository: StockJpaRepository
 ){
 
-    @Transactional
-    fun decrease(id: Long, quantity: Long) {
+    @Synchronized
+//    @Transactional
+     fun decrease(id: Long, quantity: Long) {
         val stock = stockJpaRepository.findById(id).orElseThrow()
 
         stock.decrease(quantity)
