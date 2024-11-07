684e6a287194dc6a8e98d3c6e09dec604ee1483b
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-11-07 20:29:45 +0900

    test: 동시 요청으로 Race Condition이 발생하는 테스트 작성

--- src/test/kotlin/org/example/stock/service/StockServiceTest.kt
+++ src/test/kotlin/org/example/stock/service/StockServiceTest.kt
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
 import kotlin.jvm.optionals.getOrNull
 
 @SpringBootTest
@@ -45,4 +47,29 @@ class StockServiceTest {
         assertThat(savedStock.quantity).isEqualTo(99L)
     }
 
+    @DisplayName("100번의 재고 감소 요청이 있는 경우, 재고는 0이 된다.")
+    @Test
+    fun decreaseMultipleConcurrentCall() {
+        // given
+        val threadCount = 100
+        val executorService = Executors.newFixedThreadPool(32)
+        val latch = CountDownLatch(threadCount)
+
+        // when
+        for (i in 0 until threadCount) {
+            executorService.submit {
+                try {
+                    stockService.decrease(1L, 1L)
+                } finally {
+                    latch.countDown()
+                }
+            }
+        }
+
+        latch.await()
+
+        // then
+        val stock = stockJpaRepository.findById(1L).orElseThrow()
+        assertThat(stock.quantity).isEqualTo(0L)
+    }
 }
